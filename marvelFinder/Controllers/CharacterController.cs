49d374d6c535a0f6cee41b539ba62b5a123c73a1
jose zamora <lusho911@gmail.com>
2020-08-03 09:19:44 -0600

    Mejoras en el buscador view y añadir cache

--- marvelFinder/Controllers/CharacterController.cs
+++ marvelFinder/Controllers/CharacterController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using marvelFinder.Helper;
 using marvelFinder.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Newtonsoft.Json;
 
 namespace marvelFinder.Controllers
@@ -14,31 +15,49 @@ namespace marvelFinder.Controllers
     {
         SuperHeroesAPI _api = new SuperHeroesAPI();
         Utilidades _Util = new Utilidades();
-        public async Task<IActionResult> Character(BuscarViewModel buscar)
+        private IMemoryCache _cache;
+        public CharacterController(IMemoryCache cache)
         {
-            buscar.Param = buscar.Param ?? "0";
-
-            SuperHeroViewModel.Root list = new SuperHeroViewModel.Root();
-            HttpClient client = _api.Finder(buscar.Param);
-
-            HttpResponseMessage res;
-            var data = new List<SuperHeroViewModel>();
-            res = await client.GetAsync("");
-            if (res.IsSuccessStatusCode)
-            {
-                var result = res.Content.ReadAsStringAsync().Result;
-                list = JsonConvert.DeserializeObject<SuperHeroViewModel.Root>(result);
-            }
-            if (list.response == "error")
-                return View(_Util.getNotResult());
-            else
-            {
-                if (_Util.IsNumeric(buscar.Param))
-                    return View(_Util.getResultForNumber(list));
-                else
-                    return View(list.results);
-            }
+            _cache = cache;
         }
+        public async Task<IActionResult> Character(string Param)
+        {
+            Param = Param ?? "0";
+            var PalabraBUscada = Param;
+            SuperHeroViewModel.Root list = new SuperHeroViewModel.Root();
+            HttpResponseMessage res;
+            List<SuperHeroViewModel.Result> data = new List<SuperHeroViewModel.Result>();
+
+            HttpClient client = _api.Finder(Param);
+            if (!_cache.TryGetValue(PalabraBUscada, out data))
+            {
+                if (data == null)
+                {
+                    res = await client.GetAsync("");
+                    if (res.IsSuccessStatusCode)
+                    {
+                        var result = res.Content.ReadAsStringAsync().Result;
+                        list = JsonConvert.DeserializeObject<SuperHeroViewModel.Root>(result);
+                    }
+                    if (list.response == "error")
+                        return View(_Util.getNotResult());
+                    else
+                    {
+                        if (_Util.IsNumeric(Param))
+                            data = (_Util.getResultForNumber(list));
+                        else
+                            data = (list.results);
+                    }
+                }
+                var cacheConf = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromMinutes(60));
+                _cache.Set(PalabraBUscada, data, cacheConf);
+            }
+            return View(data);
+           
+        }
+
+      
+
 
     }
 }
